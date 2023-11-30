34b6d7635b47dd6045c54a877893644a44d9a532
Bruno Gabriel <brugacag@gmail.com>
2023-11-30 05:38:49 -0300

    Layout Refatorado. Adotado LinearLayout.

--- src/main/java/devandroid/bruno/formteste/view/MainActivity.java
+++ src/main/java/devandroid/bruno/formteste/view/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
 
         controller = new Controller(MainActivity.this);
         controller.toString();
